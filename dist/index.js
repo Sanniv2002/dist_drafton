8353cfd09d8be2c4def1f3a01c73c09d1ccdacf7
Sanniv <sanniv.1020@gmail.com>
2024-05-18 22:09:56 +0530

    init

--- dist/index.js
+++ dist/index.js
@@ -395,4 +395,4 @@ const proposalTemplates = [
       Email: [Your Email]
       Phone: [Your Phone Number]`,
 ];
-app.listen(3000);
+app.listen(3000, () => console.log("Server started on port 3000"));
